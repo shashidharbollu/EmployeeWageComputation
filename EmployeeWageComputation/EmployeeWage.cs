773935dbf1ab41ac0a44076045fc8c8d05a0d323
shashidharbollu <shashidharbollu77@gmail.com>
2022-04-08 14:01:30 +0530

    [shashidhar] Add : employee wage calculation for max hours updated

--- EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -10,15 +10,19 @@ namespace EmployeeWageComputation
     {
         //constance value
         const int PART_TIME_EMP = 1, FULL_TIME_EMP = 2, EMP_WAGE_PR_HR = 20, EMP_FULL_DAY_WRKING_HR = 8, EMP_PART_TIME_WRKING_HR = 4, EMP_WORKING_PR_MONTH = 20;
-      
-       //variable values
+        
+
+        //variable values
         int empHrs = 0;
         int totalEmpSalary = 0;
+        int totalEmpHrs = 0;
+        int day = 0;
 
+        public int MAX_WORKING_HR { get; private set; }
 
         public void monthlyEmpWage()
         {
-            for (int day = 0; day <= EMP_WORKING_PR_MONTH; day++)
+            while (day <= EMP_WORKING_PR_MONTH && empHrs <= MAX_WORKING_HR)
             {
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
@@ -37,10 +41,12 @@ namespace EmployeeWageComputation
                         empHrs = 0;
                         break;
                 }
+                day++;
 
             }
+
             totalEmpSalary = empHrs * EMP_WAGE_PR_HR;
-            Console.WriteLine("One Month Employee salary is :" + totalEmpSalary);
+            Console.WriteLine("One month Employee Salary is :" + totalEmpSalary);
         }
 
 
