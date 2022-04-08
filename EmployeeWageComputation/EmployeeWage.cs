9b186a76c64d1568e24443ab197bcca7679f6370
shashidharbollu <shashidharbollu77@gmail.com>
2022-04-08 13:36:53 +0530

    [shashidhar] Add : Employee wage using switch case updated

--- EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -8,9 +8,10 @@ namespace EmployeeWageComputation
 {
     public class EmployeeWage
     {
+        //constant value
         const int PART_TIME_EMP = 1, FULL_TIME_EMP = 2, EMP_WAGE_PR_HR = 20, EMP_FULL_DAY_WRKING_HR = 8, EMP_PART_TIME_WRKING_HR = 4;
-        
-        //variable values
+
+        //variable value
         int empHrs = 0;
         int oneDayEmpSalary = 0;
 
@@ -19,20 +20,19 @@ namespace EmployeeWageComputation
             Random random = new Random();
             int empCheck = random.Next(0, 3);
 
-            if (empCheck == FULL_TIME_EMP)
+            switch (empCheck)
             {
-                Console.WriteLine("Employee Working As A full Time");
-                empHrs = EMP_FULL_DAY_WRKING_HR;
-            }
-            else if (empCheck == PART_TIME_EMP)
-            {
-                Console.WriteLine("Employee Working As Part TIme");
-                empHrs = EMP_PART_TIME_WRKING_HR;
-            }
-
-            else
-            {
-                empHrs = 0;
+                case FULL_TIME_EMP:
+                    Console.WriteLine("Employee Working As A full Time");
+                    empHrs = EMP_FULL_DAY_WRKING_HR;
+                    break;
+                case PART_TIME_EMP:
+                    Console.WriteLine("Employee Working As Part TIme");
+                    empHrs = EMP_PART_TIME_WRKING_HR;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             oneDayEmpSalary = empHrs * EMP_WAGE_PR_HR;
             Console.WriteLine("One Day Employee salary is :" + oneDayEmpSalary);
